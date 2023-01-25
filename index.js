43ebe01107da176c6401b34babebe000298a4207
Harry37x2 <bartek3737@o2.pl>
2023-01-25 23:49:56 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll('.section')
+
+const observer = new IntersectionObserver(entries=>{
+    entries.forEach(entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting)
+        //zostawia po wyświetleniu
+        if (entry.isIntersecting) observer.unobserve(entry.target)
+    })
+},
+{
+    threshold: 0,
+    // rootMargin: '-100px'
+}
+)
+
+sections.forEach(card=>{
+    observer.observe(card)
+})
